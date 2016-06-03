b40cc989473514482020c86d53f4fbbface7a18b
Térence <terencebarbe@free.fr>
2016-06-03 21:24:43 +0200

    tests middlewares & models

--- routes/recettes.js
+++ routes/recettes.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 var recettes = require('../models/recettes');
 
-router.get(['/', '/all'], recettes, function(req, res, next) {
-  console.log('routes/recettes');
+router.use(recettes);
+
+router.get(['/', '/all'], function(req, res, next) {
+  console.log('routes/recettes/all');
   res.render('recettes/all');
 });
 
 router.get('/new', function(req, res, next) {
+  res.locals.recipe = req.recipe;
+  console.log('routes/recettes/new', req.recipe);
   res.render('recettes/new');
 });
 
