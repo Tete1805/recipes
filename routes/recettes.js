0c920553264f2024a796099712d9f7d6328cfd3f
Tete1805 <terence.barbe@ing.fr>
2016-06-06 17:09:13 +0200

    changes to recettes routes

--- routes/recettes.js
+++ routes/recettes.js
@@ -2,13 +2,17 @@ var express = require('express');
 var router = express.Router();
 var recettes = require('../models/recettes');
 
-router.get(['/', '/all'], recettes, function(req, res, next) {
-  console.log('routes/recettes');
-  res.render('recettes/all', { title: 'Toutes les recettes '});
+router.use(recettes);
+
+router.get(['/', '/all'], function(req, res, next) {
+  console.log('routes/recettes/all');
+  res.render('recettes/all');
 });
 
 router.get('/new', function(req, res, next) {
-  res.render('recettes/new', { title: 'Nouvelle recette '});
+  res.locals.recipe = req.recipe;
+  console.log('routes/recettes/new', req.recipe);
+  res.render('recettes/new');
 });
 
 router.get('/my', function(req, res, next) {
