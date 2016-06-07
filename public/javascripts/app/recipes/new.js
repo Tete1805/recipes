eafc97417d2dbfd0b579635d50b3961cb0cb4040
Térence <terencebarbe@free.fr>
2016-06-07 23:54:26 +0200

    quelques trucs sur le formulaire des arômes

--- public/javascripts/app/recipes/new.js
+++ public/javascripts/app/recipes/new.js
@@ -1,7 +1,36 @@
 (function() {
 
-  window['recipes'] = window['recipes'] || {};
-  var app = window['recipes'];
-  app.new = { name: "plop" };
+  window.onload = loadAutocompletes;
 
+  function loadAutocompletes() {
+
+    var autocompletes = document.querySelectorAll(".autocomplete");
+
+    for (var i = 0; i < autocompletes.length; i++) {
+
+      var autocomplete = autocompletes[i];
+
+      var input = autocomplete.parentElement.querySelector('input')
+
+      input.addEventListener('focus', function() {
+        this.parentElement.querySelector('ul').style.display = 'inline-block';
+      }, false);
+
+      input.addEventListener('blur', function() {
+        this.value = this.parentElement.querySelector('li:hover').innerText;
+        this.parentElement.querySelector('ul').style.display = 'none';
+      }, false);
+
+      input.addEventListener('keyup', function(e){
+        var filter = this.value.toUpperCase();
+        var width = window.getComputedStyle(this.parentElement).width;
+        var suggestions = this.parentElement.querySelectorAll('li');
+        for (var i = 0; i < suggestions.length; i++) {
+          suggestions[i].style.display = suggestions[i].innerText.toUpperCase().indexOf(filter) > -1 ? 'block' : 'none';
+          suggestions[i].style.width = width;
+        }
+      })
+
+    }
+  }
 })();
