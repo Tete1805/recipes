f9de47f686fa11c450350f675ce6ca1676cf87c4
Tete1805 <terencebarbe@free.fr>
2016-06-17 22:59:04 +0200

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Recipes' });
+  res.render('site/index', { title: 'Recipes' });
 });
 
 module.exports = router;
--- routes/recettes.js
+++ routes/recettes.js
@@ -1,27 +1,27 @@
-var express = require('express');
-var router = express.Router();
-var Recette = require('../models/recette');
+const express = require('express');
+const router = express.Router();
+const Recette = require('../models/recette');
 var authRequired = require('./authRequired');
 
-router.get(['/', '/all'], function(req, res, next) {
-  Recette.find().populate('auteur').limit(10).exec(function(err, results) {
+router.get(['/', '/all', '/all/:page'], (req, res, next) => {
+  Recette.find().populate('auteur').skip((req.query.page || 0) * 10).limit(10).exec((err, results) => {
     res.render('recettes/all', { title: 'Toutes les recettes', recettes: results });
   });
 });
 
-router.get('/all/:page', function(req, res, next) {
-  Recette.find().populate('auteur').skip((req.query.page || 0) * 10).limit(10).exec(function(err, results) {
+router.get('/search/hashtag/:hashtag', (req, res, next) => {
+  Recette.find({ "hashtags" : '#' + req.params.hashtag }).populate('auteur').skip((req.query.page || 0) * 10).limit(10).exec((err, results) => {
     res.render('recettes/all', { title: 'Toutes les recettes', recettes: results });
   });
 });
 
-router.get('/detail/:id', function(req, res, next) {
+router.get('/detail/:id', (req, res, next) => {
   Recette.findOne({ "_id": req.params.id }).exec((err, result) => {
     res.render('recettes/detail', { title: 'Détail de la recette', recette: result });  
   })  
 });
 
-router.post('/detail/:id', authRequired, function(req, res, next) {
+router.post('/detail/:id', authRequired, (req, res, next) => {
   Recette.findOne({ "_id" : req.params.id }).exec((err, result) => {
      result.parse(req).save((err) => {
       if (err) { console.log('error while saving recette: ' + err); }
@@ -30,11 +30,11 @@ router.post('/detail/:id', authRequired, function(req, res, next) {
   });
 })
 
-router.get('/new', authRequired, function(req, res, next) {
+router.get('/new', authRequired, (req, res, next) => {
   res.render('recettes/new', { title: 'Nouvelle recette', recette: { nom: 'Nom de la nouvelle recette' }});   
 });
 
-router.post('/new', authRequired, function(req, res, next) {
+router.post('/new', authRequired, (req, res, next) => {
   var recette = new Recette();
   recette.parse(req).save(function(err) {
     if (err) { console.log('error while saving recette: ' + err); }
@@ -42,8 +42,8 @@ router.post('/new', authRequired, function(req, res, next) {
   res.redirect('/recettes/all');
 })
 
-router.get('/my', authRequired, function(req, res, next) {
-  Recette.find({ auteur: req.user }).populate('auteur').exec(function(err, results) {
+router.get('/my', authRequired, (req, res, next) => {
+  Recette.find({ auteur: req.user }).populate('auteur').exec((err, results) => {
     res.render('recettes/my', { title: 'Toutes mes recettes', recettes: results });
   });
 });
