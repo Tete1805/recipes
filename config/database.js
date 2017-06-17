038211f3350f2b0a93a1fd25581ce7f26649ea86
Tete1805 <terencebarbe@free.fr>
2017-06-17 18:54:23 +0200

    Paramètres de connexion mlab dans des variables d'environnement

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@
 module.exports = {
 
   // La chaîne de connexion à la base de données
-  'url' : 'mongodb://admin:poliklot@ds013270.mlab.com:13270/recipes'
+  'url' : 'mongodb://' + process.env.MLAB_USER + ':' + process.env.MLAB_PWD + '@ds013270.mlab.com:13270/recipes'
 
 };
