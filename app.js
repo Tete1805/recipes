40d7538ffa9368cce44962321c6284ad6485e4f6
Tete1805 <terencebarbe@free.fr>
2018-05-02 08:55:17 +0200

    csrf

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-app.use(csurf());
+// app.use(csurf());
 
 app.use(function(req, res, next) {
   //res.locals.csrfToken = req.csrfToken();
