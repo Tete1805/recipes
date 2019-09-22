16e8b1f6499a0e07bcb3c1bb60d3c4cc64253ccf
Tete1805 <terencebarbe@free.fr>
2019-09-22 22:43:59 +0200

    Enables extended parsing for arrays

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.set('view engine', 'pug');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('common'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compression());
 app.use(minify());
