9dde23031ffcc64be4beba87101a351231c794dd
Tete1805 <terencebarbe@free.fr>
2019-09-22 15:56:28 +0200

    Updates Bitly helper

--- config/bitly.js
+++ config/bitly.js
@@ -1,3 +1,4 @@
-var Bitly = require('bitly');
-const bitly = new Bitly('cee54271ec940460096c29dfb72644a5f3ad7be4');
+const { BitlyClient } = require('bitly');
+
+const bitly = new BitlyClient('cee54271ec940460096c29dfb72644a5f3ad7be4');
 module.exports = bitly;
