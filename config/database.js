beb33c347a4ebb010ee4b69cd32797f1e689746c
Tete1805 <terencebarbe@free.fr>
2019-09-22 22:57:11 +0200

    Changes database url to an environment parameter

--- config/database.js
+++ config/database.js
@@ -1,7 +1,5 @@
 // config/database.js
 module.exports = {
-
   // La chaîne de connexion à la base de données
-  'url' : 'mongodb://' + process.env.MLAB_USER + ':' + process.env.MLAB_PWD + '@ds013270.mlab.com:13270/recipes'
-
+  url: `mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PWD}@${process.env.MLAB_URL}/recipes`
 };
