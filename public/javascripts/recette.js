2da0f1462ce721d12019c49ae4db9a34bd9dbed7
Tete1805 <terencebarbe@free.fr>
2019-09-22 17:27:55 +0200

    Adds CSRF-Token in Like Request

--- public/javascripts/recette.js
+++ public/javascripts/recette.js
@@ -1,14 +1,19 @@
 (function() {
   window.onload = function() {
+    // Adds forEach from Array to NodeList
     if (!NodeList.prototype.forEach) {
       NodeList.prototype.forEach = Array.prototype.forEach;
     }
 
-    function clickHandler(e) {
-      var input = e.currentTarget;
+    function clickHandler(event) {
+      var input = event.currentTarget;
+      var token = input.getAttribute('csrf-token');
       fetch('/recette/' + input.attributes['recette-id'].value + '/like', {
         method: 'POST',
-        credentials: 'include'
+        credentials: 'include',
+        headers: {
+          'CSRF-Token': token // <-- is the csrf token as a header
+        }
       }).then(function(response) {
         if (response.ok) {
           var pars = response.url.split('/');
