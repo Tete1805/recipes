2ad9937eb1f3ca14710f69a625feadd0503b57cc
Tete1805 <terencebarbe@free.fr>
2019-09-22 17:55:03 +0200

    Fixes typo and formatting

--- models/arome.js
+++ models/arome.js
@@ -3,28 +3,25 @@ var mongoose = require('mongoose');
 
 // define the schema for our model
 var aromeSchema = mongoose.Schema({
-
-    marque        : String,
-    ajoute        : {
-      type        : Date,
-      default     : Date.now
-    },
-    nom           : String,
-    description   : String,
-    users         : [String]
-
+  marque: String,
+  ajoute: {
+    type: Date,
+    default: Date.now
+  },
+  nom: String,
+  description: String,
+  users: [String]
 });
 
 aromeSchema.statics.updateWithReq = function(req) {
-
   var aromesMarque = [].concat(req.body['arome-marque']);
   var aromesNom = [].concat(req.body['arome-nom']);
 
-  for (i = 0; i < aromesMarque.length; i++) {
+  for (var i = 0; i < aromesMarque.length; i++) {
     var aromeToFind = { marque: aromesMarque[i], nom: aromesNom[i] };
     this.findOneAndUpdate(aromeToFind, aromeToFind, { upsert: true }).exec();
   }
-}
+};
 
 var Arome = mongoose.model('Arome', aromeSchema);
 
