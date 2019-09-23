b2f86aee5350f7e34151a2cb9bf7c4f72550dbe3
Tete1805 <terencebarbe@free.fr>
2019-09-23 20:28:59 +0200

    Removes unused dependency

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('site/index', { title: 'Recipes' });
 });
 
