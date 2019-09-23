40a46cb1d9a4a670999a865f085e607b9fafcd58
Tete1805 <terencebarbe@free.fr>
2019-09-23 19:02:26 +0200

    Adds unlike feature on Recettes

--- public/javascripts/recette.js
+++ public/javascripts/recette.js
@@ -1,32 +1,44 @@
 (function() {
   window.onload = function() {
-    // Adds forEach from Array to NodeList
-    if (!NodeList.prototype.forEach) {
-      NodeList.prototype.forEach = Array.prototype.forEach;
-    }
-
     function clickHandler(event) {
-      var input = event.currentTarget;
-      var token = input.getAttribute('csrf-token');
-      fetch('/recette/' + input.attributes['recette-id'].value + '/like', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          'CSRF-Token': token // <-- is the csrf token as a header
-        }
-      }).then(function(response) {
-        if (response.ok) {
-          var pars = response.url.split('/');
-          var id = pars[pars.length - 2];
-          var span = document.querySelector("span[recette-id='" + id + "']");
-          span.innerText = parseInt(span.innerText) + 1;
-          span.previousSibling.className = 'liked';
-          span.previousSibling.removeEventListener('click', clickHandler);
-        }
-      });
+      const input = event.currentTarget;
+      const token = input.getAttribute('csrf-token');
+      const id = input.nextSibling.getAttribute('recette-id');
+      const nextAction = getNextAction(input.className);
+
+      if (nextAction) {
+        fetch('/recette/' + id + '/' + nextAction, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'CSRF-Token': token // <-- is the csrf token as a header
+          }
+        }).then(function(response) {
+          if (response.ok) {
+            var span = document.querySelector("span[recette-id='" + id + "']");
+            span.innerText =
+              parseInt(span.innerText) + (nextAction === 'like' ? 1 : -1);
+            span.previousSibling.className =
+              nextAction === 'like' ? 'liked' : 'like';
+          }
+        });
+      }
     }
 
-    document.querySelectorAll('.like').forEach(function(input) {
+    function getNextAction(className) {
+      switch (className) {
+        case 'liked':
+          return 'unlike';
+        case 'like':
+          return 'like';
+        default:
+          return null;
+      }
+    }
+
+    Array.from(document.querySelectorAll('.like, .liked')).forEach(function(
+      input
+    ) {
       input.addEventListener('click', clickHandler);
     });
   };
