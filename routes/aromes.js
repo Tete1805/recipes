f2baa5770531fc7b29cb4a2be9dd26cd0a26198a
Tete1805 <terencebarbe@free.fr>
2019-09-24 14:48:30 +0200

    Extracts Arome creation to service

--- routes/aromes.js
+++ routes/aromes.js
@@ -1,45 +1,37 @@
-var express = require('express');
-var router = express.Router();
-var Arome = require('../models/arome');
-var authRequired = require('./authRequired');
+const express = require('express');
+const router = express.Router();
+const aromeService = require('../services/arome');
+const authRequired = require('./authRequired');
+const NUMBER_OF_ITEMS_TO_FETCH = 50;
 
-router.get(['/', '/all'], function(req, res, next) {
-  Arome.find().sort('marque').limit(50).exec(function(err, results) {
-    res.render('aromes/all', {
-     title: 'Tous les arômes',
-     aromes: results
-   });
+router.get(['/', '/all'], async (req, res) => {
+  const aromes = await aromeService.fetch(NUMBER_OF_ITEMS_TO_FETCH);
+  res.render('aromes/all', {
+    title: 'Tous les arômes',
+    aromes: aromes
   });
 });
 
-router.get('/all/:page', function(req, res, next) {
-  Arome.find().sort('marque').skip((req.query.page || 0) * 50).limit(50).exec(function(err, results) {
-    res.render('aromes/all', {
-     title: 'Tous les arômes',
-     aromes: results
-   });
+router.get('/all/:page', async function(req, res) {
+  const skip = req.query.page || 0;
+  const aromes = await aromeService.fetch(NUMBER_OF_ITEMS_TO_FETCH, skip);
+  res.render('aromes/all', {
+    title: 'Tous les arômes',
+    aromes: aromes
   });
 });
 
-router.get('/new', authRequired, function(req, res, next) {
-  res.render('aromes/new', { title: 'Nouvel arôme '});
+router.get('/new', authRequired, function(req, res) {
+  res.render('aromes/new', { title: 'Nouvel arôme ' });
 });
 
-router.post('/new', authRequired, function(req, res, next) {
-  var arome = new Arome();
-  arome.marque = req.body.marque;
-  arome.nom = req.body.nom;
-  arome.description = req.body.description;
-  arome.save(function(err) {
-    if(err) { 
-      req.flash("Je n'ai pas réussi à sauvegarder l'arôme. =/"); 
-    }
-  });
+router.post('/new', authRequired, function(req, res) {
+  aromeService.create(req.body);
   res.redirect('/aromes/all');
-})
+});
 
-router.get('/my', authRequired, function(req, res, next) {
-  res.render('aromes/my', { title: 'Tous mes arômes '});
+router.get('/my', authRequired, function(req, res) {
+  res.render('aromes/my', { title: 'Tous mes arômes ' });
 });
 
 module.exports = router;
--- services/arome.js
+++ services/arome.js
@@ -1,7 +1,5 @@
 const Arome = require('../models/arome');
 
-module.exports = { upsert };
-
 function upsert(recette) {
   recette.aromes.forEach(async arome => {
     const { nom, marque } = arome;
@@ -14,3 +12,21 @@ function upsert(recette) {
     ).exec();
   });
 }
+
+async function fetch(limit, skip = 0) {
+  return await Arome.find()
+    .sort('marque')
+    .limit(50)
+    .skip(skip * limit)
+    .exec();
+}
+
+async function create(_arome) {
+  const arome = new Arome();
+  arome.marque = _arome.marque;
+  arome.nom = _arome.nom;
+  arome.description = _arome.description;
+  await arome.save();
+}
+
+module.exports = { create, fetch, upsert };
