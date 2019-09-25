222413862218989b16e0669787f6f897dace2544
Tete1805 <terencebarbe@free.fr>
2019-09-25 20:15:12 +0200

    Adds avatar to Profile - Model has been updated to include an avatarUrl - Profile has been updated with a field to save the avatar's url - Profile shows the aforementioned avatar or a default

--- routes/user.js
+++ routes/user.js
@@ -5,33 +5,41 @@ const Recette = require('../models/recette');
 const User = require('../models/user');
 const authRequired = require('./authRequired');
 
-router.get('/profile/:pseudo', authRequired, function(req, res, next) {
-  Promise.all([
-    User.findOne({ 'local.pseudo': req.params.pseudo }).exec(),
-    Recette.find({ 'auteur': req.params.pseudo }).exec(),
-    Arome.find({ 'users': req.params.pseudo }).exec(),
-    Recette.find({ 'likes': req.params.pseudo }).exec()
-  ]).then(function(result) {
-    res.render('users/profile', { title: 'Profil', userDetails: result[0], recettes: result[1], aromes: result[2], liked: result[3] });
-  })
+router.get('/profile/:pseudo', authRequired, async (req, res) => {
+  const { pseudo } = req.params;
+  const userDetails = await User.findOne({ 'local.pseudo': pseudo }).exec();
+  const recettes = await Recette.find({ auteur: pseudo }).exec();
+  const aromes = await Arome.find({ users: pseudo }).exec();
+  const liked = await Recette.find({ likes: pseudo }).exec();
+  res.render('users/profile', {
+    title: 'Profil',
+    userDetails,
+    recettes,
+    aromes,
+    liked
+  });
 });
 
-router.post('/profile/:pseudo', authRequired, function(req, res, next) {
-    if (req.user.local.pseudo != req.params.pseudo) {
-      throw new Error("Vous ne pouvez pas modifier un profil autre que le vôtre. =/");
-      next();
-    } else {
-      User.findOne({ 'local.pseudo': req.params.pseudo }).exec(function(err, result) {
-          result.update({ "email": req.body.email }, function(err) {
-            if (err) {
-              req.flash('error', 'error while saving comment for recette: ' + err)
-            } else {
-              req.flash('info', 'Profil sauvegardé ! :)')
-            }
-            res.redirect('/user/profile/' + req.user.local.pseudo);
-          })
-        })
-      }
+router.post('/profile/:pseudo', authRequired, async (req, res) => {
+  const { pseudo } = req.params;
+  const { pseudo: currentUser } = req.user.local;
+  if (currentUser != pseudo) {
+    throw new Error(
+      'Vous ne pouvez pas modifier un profil autre que le vôtre. =/'
+    );
+  } else {
+    try {
+      const user = await User.findOne({ 'local.pseudo': pseudo }).exec();
+      await user.update({
+        email: req.body.email,
+        avatarUrl: req.body.avatarUrl
+      });
+    } catch (e) {
+      req.flash('error', "Je n'ai pas réussi à sauver le profil. \n" + e);
+    }
+    req.flash('info', 'Profil sauvegardé ! :)');
+    res.redirect('/user/profile/' + pseudo);
+  }
 });
 
 module.exports = router;
