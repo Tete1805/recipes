43017d287443d668337fa0d08845d88876a6f6c2
Tete1805 <terencebarbe@free.fr>
2019-09-25 12:12:23 +0200

    Refactors List service I didn't like the previous service This one feels more robust, obvious and clean

--- routes/recettes.js
+++ routes/recettes.js
@@ -1,14 +1,12 @@
 var express = require('express'),
   router = express.Router(),
-  Recette = require('../models/recette'),
   listService = require('../services/list'),
   searchService = require('../services/search');
 
 router.get(['/', '/all/:page'], async (req, res) => {
-  const recettes = listService.list('recettes');
   const page = parseInt(req.params.page, 10);
-  const results = await recettes.getPage(page);
-  res.render('recettes/all', { recettes: results, page });
+  const recettes = await new listService('recettes').setPage(page).get();
+  res.render('recettes/all', { recettes, page });
 });
 
 router.get(
@@ -16,17 +14,14 @@ router.get(
     '/search/:searchField/:searchString/',
     '/search/:searchField/:searchString/:page'
   ],
-  (req, res) => {
+  async (req, res) => {
     const filter = searchService.getFilterFromParams(req.params);
-    Recette.find(filter)
-      .skip(10 * (parseInt(req.params.page, 10) - 1 || 0))
-      .limit(10)
-      .exec((err, results) => {
-        res.render('recettes/all', {
-          recettes: results,
-          page: req.params.page
-        });
-      });
+    const page = parseInt(req.params.page, 10);
+    const recettes = await new listService('recettes')
+      .setFilter(filter)
+      .setPage(page)
+      .get();
+    res.render('recettes/all', { recettes, page });
   }
 );
 
--- services/list.js
+++ services/list.js
@@ -1,28 +1,55 @@
 const Arome = require('../models/arome'),
-  Recette = require('../models/recette');
+  Recette = require('../models/recette'),
+  User = require('../models/user');
 
 const MAX_RECIPES_PER_PAGE = 8;
+const MAX_AROMAS_PER_PAGE = 50;
+const MAX_USERS_PER_PAGE = 50;
 
-function list(type) {
-  switch (type) {
-    case 'recettes':
-      return listRecettes();
-    case 'aromes':
-      return Arome.find();
-    case 'users':
-      return [];
+class listService {
+  constructor(type) {
+    this.model = getModel(type);
+    this.max_items_per_page = getMaxItemsPerPage(type);
+    this.filter = { nom: { $exists: true } };
+    this.page = 1;
+  }
+  setFilter(filter) {
+    this.filter = Object.assign(this.filter, filter);
+    return this;
+  }
+  setPage(page) {
+    this.page = page;
+    return this;
+  }
+  async get() {
+    return await this.model
+      .find(this.filter)
+      .skip((this.page - 1) * this.max_items_per_page)
+      .limit(this.max_items_per_page)
+      .exec();
   }
 }
 
-function listRecettes() {
-  return {
-    getPage: async page => {
-      return await Recette.find({ nom: { $exists: true } })
-        .skip((page - 1) * MAX_RECIPES_PER_PAGE)
-        .limit(MAX_RECIPES_PER_PAGE)
-        .exec();
-    }
-  };
+function getModel(type) {
+  switch (type) {
+    case 'recettes':
+      return Recette;
+    case 'aromes':
+      return Arome;
+    case 'users':
+      return User;
+  }
 }
 
-module.exports = { list };
+function getMaxItemsPerPage(type) {
+  switch (type) {
+    case 'recettes':
+      return MAX_RECIPES_PER_PAGE;
+    case 'aromes':
+      return MAX_AROMAS_PER_PAGE;
+    case 'users':
+      return MAX_USERS_PER_PAGE;
+  }
+}
+
+module.exports = listService;
