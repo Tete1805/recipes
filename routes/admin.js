fff78a6d5ab70fe7d01305522e8707e384326349
Tete1805 <terencebarbe@free.fr>
2019-09-25 12:22:56 +0200

    Refactors admin route to use List service

--- routes/admin.js
+++ routes/admin.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const Arome = require('../models/arome');
-const Recette = require('../models/recette');
+const listService = require('../services/list');
 const User = require('../models/user');
 const adminRequired = require('./adminRequired');
 
-router.get('/recettes', adminRequired, function(req, res, next) {
-  Recette.find().exec((err, results) => {
-    res.render('admin/recettes', { title: 'Admin recettes', recettes: results });
-  })
+router.get('/recettes', adminRequired, async (req, res) => {
+  const recettes = await new listService('recettes').setLimit(0).get();
+  res.render('admin/recettes', {
+    title: 'Admin recettes',
+    recettes
+  });
 });
 
-router.get('/users', adminRequired, function(req, res, next) {
-  User.find().sort('level').sort('ajoute').exec((err, results) => {
-    res.render('admin/users', { title: 'Admin users', users: results });
-  })
+router.get('/users', adminRequired, function(req, res) {
+  User.find()
+    .sort('level')
+    .sort('ajoute')
+    .exec((err, results) => {
+      res.render('admin/users', { title: 'Admin users', users: results });
+    });
 });
 
-router.get('/aromes', adminRequired, function(req, res, next) {
-  Arome.find().sort('marque').exec((err, results) => {
-    res.render('admin/aromes', { title: 'Admin aromes', aromes: results });
-  })
+router.get('/aromes', adminRequired, async (req, res) => {
+  const aromes = await new listService('aromes').setLimit(0).get();
+  res.render('admin/aromes', { title: 'Admin aromes', aromes });
 });
 
 module.exports = router;
