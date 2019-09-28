472083b48003e39fbc8186ab526a68dbfa34651f
Tete1805 <terencebarbe@free.fr>
2019-09-28 16:32:15 +0200

    Adds a 404 catcher specific to the API

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,4 +6,8 @@ const list = require('./list');
 router.use('/recette', recette);
 router.use('/list', list);
 
+router.use((req, res) => {
+  res.status(404).json({ data: "This route doesn't exist in the API." });
+});
+
 module.exports = router;
